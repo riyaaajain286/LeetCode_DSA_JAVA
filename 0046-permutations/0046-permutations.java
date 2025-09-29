89a48c1e803ebf8991d9a6d14ce1262037daae41
Riya Jain <riyaaajain286@gmail.com>
2025-09-29 19:19:11 +0530

    Time: 1 ms (92.78%), Space: 44.8 MB (18.96%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,23 +1,29 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-         List<List<Integer>> ans=new ArrayList<>();
-         boolean[] freq=new boolean[nums.length];
-         helper(nums,ans,new ArrayList<>(),freq);
-         return ans;
+        List<List<Integer>> outer=new ArrayList<>();
+    return helper(nums,0,outer);
     }
-    public void helper(int[] nums,List<List<Integer>> ans,List<Integer> list,boolean[] freq){
-        if(list.size()==nums.length){
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i]=true;
-                list.add(nums[i]);
-                helper(nums,ans,list,freq);
-                list.remove(list.size()-1);
-                freq[i]=false;
-            }
-        }
-    }
+    public static List<List<Integer>> helper(int[]nums,int ind,List<List<Integer>> outer){
+     List<Integer> inner=new ArrayList<>();
+     if(ind>=nums.length){
+      for(int i=0;i<nums.length;i++){
+        inner.add(nums[i]);
+      }
+      outer.add(inner);
+     return outer;
+     }
+     for(int i=ind;i<nums.length;i++){
+      swap(nums,i,ind);
+      helper(nums,ind+1,outer);
+      swap(nums,i,ind);
+     }
+       return outer;
+  }
+
+  
+  public static void swap(int[] nums,int first,int second){
+    int temp=nums[first];
+    nums[first]=nums[second];
+    nums[second]=temp;
+  }
 }
