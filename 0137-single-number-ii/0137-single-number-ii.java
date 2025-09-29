f3a4bb73907894731d33d66501b8db4224395dea
Riya Jain <riyaaajain286@gmail.com>
2025-09-29 18:55:48 +0530

    Time: 1 ms (80.75%), Space: 45.4 MB (67.77%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+      int ones=0;
+    int twos=0;
+    for(int i=0;i<nums.length;i++){
+      ones=(nums[i]^ones)&(~twos);
+      twos=(nums[i]^twos)&(~ones);
+     }
+    return ones;
+        
+    }
+}
