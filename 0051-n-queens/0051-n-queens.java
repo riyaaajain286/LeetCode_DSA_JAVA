ecf8ff880b91b379d3e3a7fbd3a8124932a02377
Riya Jain <riyaaajain286@gmail.com>
2025-10-04 20:48:34 +0530

    Time: 5 ms (28.79%), Space: 45.1 MB (39.32%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,73 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+    List<List<String>> allboards=new ArrayList<>();
+    char board[][]=new char[n][n];
+    helper(board,allboards,0);
+    return allboards;
+  }
+  public void helper(char[][] board,List<List<String>> allboards,int col){
+    if(col==board.length){
+      saveBoard(board,allboards);
+      return;
+    }
+    for(int row=0;row<board.length;row++){
+      if(isSafe(row,col,board)){
+        board[row][col]='Q';
+        helper(board,allboards,col+1);
+         board[row][col]='.';
+      }
+    }
+  }
+  public boolean isSafe(int row,int col,char[][] board){
+    //horizontal
+    for(int j=0;j<board.length;j++){
+      if(board[row][j]=='Q'){
+        return false;
+      }
+    }
+    //vertical
+    for(int i=0;i<board[0].length;i++){
+      if(board[i][col]=='Q'){
+        return false;
+      }
+    }
+    //upper left
+    for(int r=row-1,c=col-1;r>=0&&c>=0;r--,c--){
+      if(board[r][c]=='Q')
+      return false;
+    }
+    //upper right
+    //  for(int r=row,c=col;r>=0&&c<board.length;r--,c++){
+    //   board[r][c]='Q';
+    //   return false;
+    // }
+     //lower left
+    for(int r=row+1,c=col-1;r<board.length&&c>=0;r++,c--){
+       if(board[r][c]=='Q')
+      return false;
+    }
+     //lower right
+    // for(int r=row,c=col;r<board.length&&c<board.length;r++,c++){
+    //   board[r][c]='Q';
+    //   return false;
+    // }
+    return true;
+  }
+  public void saveBoard(char[][] board,List<List<String>> allboards){
+    String row="";
+    List<String> newboard=new ArrayList<>();
+    for(int i=0;i<board.length;i++){
+      row="";
+      for(int j=0;j<board[0].length;j++){
+        if(board[i][j]=='Q'){
+          row+='Q';
+        }
+        else{
+          row+='.';
+        }
+      }
+      newboard.add(row);
+    }
+    allboards.add(newboard);
+  }
+  }
