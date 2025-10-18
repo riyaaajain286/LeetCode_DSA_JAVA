c4eaeb446c326bac12b1939fe8c4ff07f8d6f270
Riya Jain <riyaaajain286@gmail.com>
2025-10-18 13:30:13 +0530

    Time: 10 ms (95.69%), Space: 49 MB (33.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,25 +1,28 @@
 class Solution {
-  class Pair{
-    int number;
-    int freq;
-    public Pair(int number,int freq){
-    this.number=number;
-    this.freq=freq;
-  }
-  }
     public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer,Integer> freqmap=new HashMap<>();
-        for(int n:nums){
-        freqmap.put(n,freqmap.getOrDefault(n,0)+1);
+    Map<Integer,Integer> freqmap=new HashMap<>();
+    for(int n:nums){
+      freqmap.put(n,freqmap.getOrDefault(n,0)+1);
+    }
+       List<Integer>[] buckets=new ArrayList[nums.length+1];
+    for(int key:freqmap.keySet()){
+      int c=freqmap.get(key);
+      if(buckets[c]==null){
+      buckets[c]=new ArrayList<>();
+      }
+      buckets[c].add(key);
+    }
+    int[] res=new int[k];
+    int idx=0;
+    for(int i=nums.length;i>=0&&idx<k;i--){
+      if(buckets[i]!=null){
+        for(int num:buckets[i]){
+        res[idx++]=num;
+        if(idx==k) break;
         }
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->b.freq-a.freq);
-        for(Map.Entry<Integer,Integer> entry:freqmap.entrySet()){
-        pq.add(new Pair(entry.getKey(),entry.getValue()));
-        }
-        int res[]=new int[k];
-        for(int i=0;i<k;i++){
-        res[i]=pq.poll().number;
-        }
-    return res;
+      } 
+    }
+
+  return res;
+    }
 }
-  }
