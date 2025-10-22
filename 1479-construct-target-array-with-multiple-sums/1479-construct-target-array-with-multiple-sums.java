1725dec2114040099a53f92d684dbbd1e285a0f3
Riya Jain <riyaaajain286@gmail.com>
2025-10-22 21:30:33 +0530

    Time: 9 ms (68.69%), Space: 50 MB (97.98%) - LeetHub

--- 1479-construct-target-array-with-multiple-sums/1479-construct-target-array-with-multiple-sums.java
+++ 1479-construct-target-array-with-multiple-sums/1479-construct-target-array-with-multiple-sums.java
@@ -1,32 +1,33 @@
-class Solution {
+class Solution { 
     public boolean isPossible(int[] target) {
-        PriorityQueue<Long> pq=new PriorityQueue<>(Collections.reverseOrder());
-        long allsum=0;
-        for(long i:target){
-            pq.add(i);
-            allsum+=i;
-        }
-      
-        long remsum=0;
-        long element=0;
-      
-        //maxelement=remsum+element
-        while(true){
-              long maxelement=pq.poll();
-           remsum=allsum-maxelement;
-           //edge cases
-           if(maxelement==1||remsum==1){
+     PriorityQueue<Long> pq=new PriorityQueue<>(Collections.reverseOrder()); 
+     long allsum=0;
+      for(long i:target){ 
+        pq.add(i); 
+        allsum+=i;
+         } 
+       
+         long remsum=0;
+          long element=0; //maxelement=remsum+element 
+          while(pq.peek()!=1){
+              long maxelement=pq.poll(); 
+             remsum=allsum-maxelement;
+              //edge cases
+               if(remsum<=0||remsum>=maxelement){
+                return false;
+                }
+                element=maxelement%remsum; 
+                if(element==0){ 
+                    if(remsum!=1) {
+                         return false;
+                    } 
+                          else{
+                             return true;
+                        }
+                }
+                 allsum=remsum+element;
+                  pq.add(element);
+            }
             return true;
-           }
-           if(remsum==0||maxelement<remsum||maxelement%remsum==0){
-            return false;
-           }
-          
-           element=(int)(maxelement%remsum);
-           pq.add(element);
-           allsum=remsum+element;
-         
         }
-  
     }
-}
