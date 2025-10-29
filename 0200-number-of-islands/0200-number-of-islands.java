85ea4a83615dc396a6dfc96493f79d419ea39e97
Riya Jain <riyaaajain286@gmail.com>
2025-10-29 08:47:54 +0530

    Time: 5 ms (32%), Space: 51.6 MB (25.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,50 +1,44 @@
 class Solution {
-    class Pair{
-        int first;
-        int second;
-        public Pair(int first,int second){
-            this.first=first;
-            this.second=second;
-        }
-    }
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        int count=0;
-        int vis[][]=new int[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'&&vis[i][j]==0){
-                     count++;
-                    bfs(vis,grid,i,j);
-                   
+      int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+
+        int[][] vis = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // If land ('1') and not yet visited, it's a new island
+                if (grid[i][j] == '1' && vis[i][j] == 0) {
+                    count++;
+                    dfs(i, j, grid, vis);
                 }
             }
         }
+
         return count;
     }
-    private void bfs(int[][] vis,char[][] grid,int ro,int co){
-        vis[ro][co]=1;
-        Queue<Pair> q=new LinkedList<>();
-        int n=grid.length;
-        int m=grid[0].length;
-        q.add(new Pair(ro,co));
-          int[] delRow = {-1, 0, 1, 0};
+
+    private void dfs(int row, int col, char[][] grid, int[][] vis) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        vis[row][col] = 1;
+
+        // 4 possible directions: up, right, down, left
+        int[] delRow = {-1, 0, 1, 0};
         int[] delCol = {0, 1, 0, -1};
-        while(!q.isEmpty()){
-            int row=q.peek().first;
-            int col=q.peek().second;
-            q.remove();
-             for (int i = 0; i < 4; i++) {
-                int nrow = row + delRow[i];
-                int ncol = col + delCol[i];
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0){                   
-                        vis[nrow][ncol]=1;
-                    q.add(new Pair(nrow,ncol));
-                }
+
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + delRow[i];
+            int ncol = col + delCol[i];
+
+            // Check valid coordinates and unvisited land
+            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                grid[nrow][ncol] == '1' && vis[nrow][ncol] == 0) {
+                dfs(nrow, ncol, grid, vis);
             }
-          
-        } 
-    }
+        }
+    }  
+    
 }
