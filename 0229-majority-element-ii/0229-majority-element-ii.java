016445d191158a143fe12648e52fbed891c0f2d0
Riya Jain <riyaaajain286@gmail.com>
2025-10-31 23:11:36 +0530

    Time: 4 ms (61.52%), Space: 52.5 MB (5.08%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,49 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n=nums.length;
-        int ans=((int)n/3)+1;
-        List<Integer> anslist=new ArrayList<>();
-        Map<Integer,Integer> map=new HashMap<>();
+        int c1=0,c2=0;
+        int e1=Integer.MIN_VALUE;
+        int e2=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(map.get(nums[i])==ans){
-                anslist.add(nums[i]);
+            if(c1==0&&e2!=nums[i]){
+                c1++;
+                e1=nums[i];
             }
-            if(anslist.size()==2){
-                return anslist;
+            else if(c2==0&&e1!=nums[i]){
+                c2++;
+                e2=nums[i];
+            }
+            else if(nums[i]==e1){
+                c1++;
+            }
+            else if(nums[i]==e2){
+                c2++;
+            }
+            else{
+                c1--;
+                c2--;
             }
         }
-        return anslist;
+    c1=0;
+    c2=0;
+    ArrayList<Integer> anslist=new ArrayList<>();
+    for(int i=0;i<n;i++){
+     if(nums[i]==e1){
+        c1++;
+     }
+     else if(nums[i]==e2){
+        c2++;
+     }
     }
+     int ans=((int)n/3)+1;
+     if(c1>=ans){
+        anslist.add(e1);
+     }
+      if(c2>=ans){
+        anslist.add(e2);
+     }
+     return anslist;
+    }
+    
+
 }
