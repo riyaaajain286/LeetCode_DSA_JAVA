01f15275f301aafe588e3f9f052d1f86b29d7865
Riya Jain <riyaaajain286@gmail.com>
2025-11-04 19:34:10 +0530

    Time: 20 ms (20.56%), Space: 45.9 MB (5.69%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,25 +1,31 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans=new ArrayList<>();
-        Arrays.sort(nums);
         int n=nums.length;
-        for(int i=0;i<n;i++){
+        if(n<4) return ans;
+        Arrays.sort(nums);
+        for(int i=0;i<n-3;i++){
             if(i>0&&nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n;j++){
-                if(j>i+1&&nums[j]==nums[j-1]) continue;
-               for(int k=j+1;k<n;k++){
-                if(k>j+1&&nums[k]==nums[k-1]) continue;
-                for(int l=k+1;l<n;l++){
-                    if(l>k+1&&nums[l]==nums[l-1]) continue;
-                     long sum=nums[i]+nums[j];
-                    sum+=nums[k];
-                    sum+=nums[l];
-                    if(sum==target){
-                       
-                        ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                    }
+            for(int j=i+1;j<n-2;j++){
+               if(j>i+1&&nums[j]==nums[j-1]) continue;
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                long sum=nums[i]+nums[j];
+                sum+=nums[k];
+                sum+=nums[l];
+                if(sum==target){
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                    k++;
+                    l--;
+                    //  Skip duplicates only after finding a valid quadruplet
+                    while(k<l&&nums[k]==nums[k-1]) k++;
+                    while(k<l&&nums[l]== nums[l+1]) l--;
                 }
-              }
+                else if(sum<target) k++;
+                else l--;
+             
+                }
             }
         }
         return ans;
