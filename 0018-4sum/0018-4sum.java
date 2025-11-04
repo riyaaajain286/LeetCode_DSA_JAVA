45cf4e7fc09bce9e9fb29b9882aaeaf6c7bc9299
Riya Jain <riyaaajain286@gmail.com>
2025-11-04 22:32:39 +0530

    Time: 272 ms (5.02%), Space: 47.4 MB (5.69%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -3,9 +3,11 @@ class Solution {
         List<List<Integer>> ans=new ArrayList<>();
         int n=nums.length;
        if(n<4) return ans;
-       
+       Arrays.sort(nums);
         for(int i=0;i<n;i++){
+            if(i>0&&nums[i]==nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
+                if(j>i+1&&nums[j]==nums[j-1]) continue;
                 Set<Long> set=new HashSet<>();
                  for(int k=j+1;k<n;k++){
                     long sum=(long)nums[i]+(long)nums[j]+(long)nums[k];
