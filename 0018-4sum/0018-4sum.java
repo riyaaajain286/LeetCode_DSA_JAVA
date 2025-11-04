d9dcbd667ac7e6aea162b440c3183a1ff9385ca3
Riya Jain <riyaaajain286@gmail.com>
2025-11-04 22:30:55 +0530

    Time: 754 ms (5.02%), Space: 47.3 MB (5.69%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -2,30 +2,23 @@ class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> ans=new ArrayList<>();
         int n=nums.length;
-        if(n<4) return ans;
-        Arrays.sort(nums);
-        for(int i=0;i<n-3;i++){
-            if(i>0&&nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n-2;j++){
-               if(j>i+1&&nums[j]==nums[j-1]) continue;
-                int k=j+1;
-                int l=n-1;
-                while(k<l){
-                long sum=nums[i]+nums[j];
-                sum+=nums[k];
-                sum+=nums[l];
-                if(sum==target){
-                    ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
-                    k++;
-                    l--;
-                    //  Skip duplicates only after finding a valid quadruplet
-                    while(k<l&&nums[k]==nums[k-1]) k++;
-                    while(k<l&&nums[l]== nums[l+1]) l--;
-                }
-                else if(sum<target) k++;
-                else l--;
-             
-                }
+       if(n<4) return ans;
+       
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                Set<Long> set=new HashSet<>();
+                 for(int k=j+1;k<n;k++){
+                    long sum=(long)nums[i]+(long)nums[j]+(long)nums[k];
+                    long fourth=(long)target-sum;
+                    if(set.contains(fourth)){
+                       List<Integer> quad=Arrays.asList(nums[i],nums[j],nums[k],(int)fourth);
+                       Collections.sort(quad);
+                       if(!ans.contains(quad)){
+                        ans.add(quad);
+                       }
+                    }
+                         set.add((long)nums[k]);
+                 }
             }
         }
         return ans;
