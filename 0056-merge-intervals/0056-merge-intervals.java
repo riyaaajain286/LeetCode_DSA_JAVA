0738468a6b892b6f504972dc8c6181ecbe9503c0
Riya Jain <riyaaajain286@gmail.com>
2025-11-17 20:08:26 +0530

    Time: 9 ms (41.3%), Space: 48.5 MB (24.27%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,16 +5,15 @@ class Solution {
         int[][] ans=new int[n][2];
         int index=0;
         for(int i=0;i<n;i++){
-            int start=intervals[i][0];
-            int end=intervals[i][1];
-            int j=i+1;
-           while(j<n&&intervals[j][0]<=end){
-                end=Math.max(end,intervals[j][1]);
-                j++;
+            //if no interval added or no overlap add new interval
+            if(index==0||intervals[i][0]>ans[index-1][1]){
+                ans[index++]=new int[]{intervals[i][0],intervals[i][1]};
             }
-            ans[index++]=new int[]{start,end};
-            i=j-1;
+            else{
+                //merge intervals by updating end value
+                ans[index-1][1]=Math.max(intervals[i][1],ans[index-1][1]);
+            }
         }
-       return Arrays.copyOf(ans,index);
+        return Arrays.copyOf(ans,index);
     }
 }
