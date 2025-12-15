d06966e0b9e01e3e4089054a52d282de8f95a981
Riya Jain <riyaaajain286@gmail.com>
2025-12-15 17:49:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -1,19 +1,21 @@
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
-     int c=0;
-    int max=0;
-    for(int i:nums){
-      if(i==1){
-        c++;
-        // if(c>max){
-        //   max++;
-        // }
-        max=Math.max(max,c);
-      }
-      else{
-        c=0;
-      }
-    }
-    return max;   
+        int c=0;
+        int max=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                c++;
+                i++;
+                while(i<n){
+                    if(nums[i]==1){
+                        c++;
+                        i++;
+                    }
+                }
+                max=Math.max(c,max);
+            }
+        }
+        return max;
     }
 }
