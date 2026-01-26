2f9cb989070b30974f885fbc3fed7326091dd3ea
Riya Jain <riyaaajain286@gmail.com>
2026-01-26 20:17:06 +0530

    Time: 7 ms (71.26%), Space: 46.5 MB (60.84%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -18,13 +18,13 @@ class Solution {
     //     dp[0][j]=0;
     //    }
     int dp[][]=new int[n+1][m+1];
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=m;j++){
-            if(i==0||j==0){
-                dp[i][j]=0;
-            }
-        }
-    }
+    // for(int i=0;i<=n;i++){
+    //     for(int j=0;j<=m;j++){
+    //         if(i==0||j==0){
+    //             dp[i][j]=0;
+    //         }
+    //     }
+    // }
 
        //iteration
        for(int i=1;i<=n;i++){
