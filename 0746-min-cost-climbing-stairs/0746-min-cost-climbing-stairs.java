84d020d2e70f63c792f3b112f01b64ab5fd0af39
Riya Jain <riyaaajain286@gmail.com>
2026-02-11 11:51:47 +0530

    Time: 0 ms (100%), Space: 44.9 MB (57.35%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,23 +1,18 @@
 class Solution {
-    //memoization
+    //bottom up
     int[] dp;
     public int minCostClimbingStairs(int[] cost) {
         int n=cost.length;
-        if(n==0) return 0;
-        if(n==1) return cost[0];
-        dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        int option1=helper(cost,0);
-        int option2=helper(cost,1);
-        return Math.min(option1,option2);
-    }
-    public int helper(int[] cost,int i){
-        if(i>=cost.length) return 0;
-        //memo check
-        if(dp[i]!=-1) return dp[i];
-        int step1=cost[i]+helper(cost,i+1);
-        int step2=cost[i]+helper(cost,i+2);
-        dp[i]=Math.min(step1,step2);
-        return dp[i];
+        dp=new int[n+2];
+        //initialization
+        dp[n]=0;
+        dp[n+1]=0;
+        //iteration
+        for(int i=n-1;i>=0;i--){
+            dp[i]=cost[i]+Math.min(dp[i+1],dp[i+2]);
+           
+        }
+        //answer can be any between 0 or 1 coz start may be from 0 or 1 as given in ques
+         return Math.min(dp[0],dp[1]);
     }
 }
