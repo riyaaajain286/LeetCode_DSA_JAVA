c5fb95c2df52faac1aa6adb1cad4ed403db0c23b
Riya Jain <riyaaajain286@gmail.com>
2026-02-11 12:28:19 +0530

    Time: 0 ms (100%), Space: 43 MB (15.98%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,19 @@
 class Solution {
+    //memoization
+    int[] dp;
     public int rob(int[] nums) {
-   int n=nums.length;
-   if(n==1){
-    return nums[0];
-   }
-      //bottom-up approach
-   int [] arr=new int[n+1];
-  //arr[i] means max stolen money till ith house
-   //if no house means i=0
-   arr[0]=0;
-   //1 house
-   arr[1]=nums[0];
-   for(int i=2;i<=n;i++){
-    int steal=nums[i-1]+arr[i-2];
-    int skip=arr[i-1];
-    arr[i]=Math.max(steal,skip);
-   }
-   return arr[n];
-}
+        dp=new int[nums.length+1];
+        Arrays.fill(dp,-1);
+        int ans=helper(nums,0);
+        return ans;
+    }
+    public int helper(int[] nums,int i){
+        if(i>=nums.length) return 0;
+        //memocheck
+        if(dp[i]!=-1) return dp[i];
+        int option1=nums[i]+helper(nums,i+2);//rob
+        int option2=0+helper(nums,i+1);//skip
+        dp[i]= Math.max(option1,option2);
+        return dp[i];
+    }
 }
