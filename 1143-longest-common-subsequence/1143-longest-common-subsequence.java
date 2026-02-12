4511a1249c32b4cc47ba38f15e22ee0ff04a586a
Riya Jain <riyaaajain286@gmail.com>
2026-02-12 11:56:38 +0530

    Time: 59 ms (5.31%), Space: 109.3 MB (5.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,39 +1,20 @@
 class Solution {
-     int[][] dp;
+    int[][] dp;
     public int longestCommonSubsequence(String text1, String text2) {
-        int n=text1.length();
-        int m=text2.length();
-        dp=new int[n+1][m+1];
-        int ans=lcs(text1,text2,n,m);
-        return ans;
+        dp=new int[1001][1001];
+        for(int row[]:dp)
+          Arrays.fill(row,-1);
+        return lcs(text1,text2,text1.length(),text2.length());
     }
-    public int lcs(String text1, String text2,int n,int m){
-      //initialization
-    //    for(int i=0;i<=n;i++){
-    //     dp[i][0]=0;
-    //    }
-    //    for(int j=0;j<=m;j++){
-    //     dp[0][j]=0;
-    //    }
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=m;j++){
-            if(i==0||j==0){
-                dp[i][j]=0;
-            }
-        }
-    }
-
-       //iteration
-       for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-         if(text1.charAt(i-1)==text2.charAt(j-1)){
-            dp[i][j]= 1+dp[i-1][j-1];
+    public int lcs(String a,String b,int n,int m){
+        if(n==0 || m==0) return 0;
+         if(dp[n][m]!=-1) return dp[n][m];
+        if(a.charAt(n-1)==b.charAt(m-1)){
+            dp[n][m]= 1+lcs(a,b,n-1,m-1);
         }
         else{
-            dp[i][j]= Math.max(dp[i][j-1],dp[i-1][j]);
+            dp[n][m]= Math.max(lcs(a,b,n,m-1),lcs(a,b,n-1,m));
         }
-          }
-       }
         return dp[n][m];
     }
 }
