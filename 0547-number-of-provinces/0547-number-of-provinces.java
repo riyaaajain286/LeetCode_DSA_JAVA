42aeb4b5e722bf41424dbf3b59c99f408019d2f2
Riya Jain <riyaaajain286@gmail.com>
2026-02-19 22:37:37 +0530

    Time: 0 ms (100%), Space: 47.5 MB (45.46%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,59 +1,22 @@
-class Disjoint{
- int[] parent;
- int[] rank;
-  public Disjoint(int n){
-    parent=new int[n];
-    rank=new int[n];
-    for(int i=0;i<n;i++){ 
-      parent[i]=i;
-      rank[i]=0;
-    }
-  }
-    //parent and compress path also
-    public  int findparent(int node){
-      if(parent[node]==node)  return node;
-
-      //path compression step
-      parent[node]=findparent(parent[node]);
-      return parent[node];
-
-    }
-    //union by rank
-    public   void unionByRank(int u,int v){
-    int pu=findparent(u);
-    int pv=findparent(v);
-    if(pu==pv) return;//already same set
-// Attach smaller rank tree under larger rank tree
-    if(rank[pu]<rank[pv]){
-      parent[pu]=pv;
-    }
-    else if(rank[pv]<rank[pu]){
-      parent[pv]=pu;
-    }
-    else{
-      parent[pv]=pu;
-      rank[pu]++;// Increase rank if both equal 
-    }
-    }
-}
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        if(isConnected==null||isConnected.length==0)  return 0;
-        int V=isConnected.length;
-        Disjoint ds=new Disjoint(V);
-        for(int i=0;i<V;i++){
-            for(int j=i+1;j<V;j++){
-                if(isConnected[i][j]==1){
-                    ds.unionByRank(i,j);
-                }
-            }
+        int n=isConnected.length;
+        boolean[] vis=new boolean[n];
+        int provinces=0;
+        for(int i=0;i<n;i++){// traverse every   node
+          if(vis[i]==false){
+            dfs(isConnected,vis,i);
+            provinces++;
         }
-        int c=0;
-        for(int i=0;i<V;i++){
-            if(ds.findparent(i)==i){
-                c++;
-            }
         }
-        return c;
+        return provinces;
+    }
+    public void dfs(int[][] isConnected,boolean[] vis,int node){
+        int n=isConnected.length;
+        vis[node]=true;
+        for(int j=0;j<n;j++){//check every cell
+          if(isConnected[node][j]==1 && vis[j]==false)
+             dfs(isConnected,vis,j);
+        }
     }
 }
