8e2a3e77d36ed44ade74ea96939f1297b4dcd0a9
Riya Jain <riyaaajain286@gmail.com>
2026-02-23 11:51:25 +0530

    Time: 3 ms (99.4%), Space: 90.8 MB (87.13%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -1,45 +1,28 @@
 class Solution {
-    int[] dr={-1,0,1,0};
-    int[] dc={0,1,0,-1};
-    int mr,mc,maxr,maxc;
     public int[][] findFarmland(int[][] land) {
-       List<int[]> list=new ArrayList<>();
-        int n=land.length;
-        int m=land[0].length;
-        boolean[][] vis=new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(land[i][j]==1 && !vis[i][j]){
-                    // initialize boundaries
-                    mr=i;
-                    mc=j;
-                    maxr=i;
-                    maxc=j;
-                    dfs(land,vis,i,j);
-                    list.add(new int[]{mr,mc,maxr,maxc});
-                }
-            }
-        }
-        return list.toArray(new int[list.size()][]);
-    }
-    
-    
-    public void dfs(int[][] land,boolean[][] vis,int sr,int sc){
-         vis[sr][sc]=true;
-         int n=land.length;
-        int m=land[0].length;
-         // update rectangle boundaries
-         mr=Math.min(mr,sr);
-         mc=Math.min(mc,sc);
-         maxr=Math.max(maxr,sr);
-         maxc=Math.max(maxc,sc);
-         for(int k=0;k<4;k++){
-            int nr=sr+dr[k];
-            int nc=sc+dc[k];
-            if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && land[nr][nc]==1){
-                dfs(land,vis,nr,nc);
-            }
+        //Every farmland group is a perfect rectangle.
+    int n=land.length;
+    int m=land[0].length;
+    List<int[]> list=new ArrayList<>();
+    //check topmost
+    for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+        if((land[i][j]==1) && (i==0 ||land[i-1][j]==0) && (j==0||land[i][j-1]==0)){
+          int r=i;
+          int c=j;
+          //move down
+         while(r<n && land[r][j]==1 ){
+           r++;
          }
-
+         //move right
+          while(c<m && land[i][c]==1 ){
+            c++;
+          }
+          //taking n-1 coz we stop at n when its =0 so take n-1 for land
+          list.add(new int[]{i,j,r-1,c-1});
+        }
+      }
+    }
+    return list.toArray(new int[list.size()][]);
     }
 }
