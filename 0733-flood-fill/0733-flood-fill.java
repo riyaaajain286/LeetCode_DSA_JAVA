baab36b63195037b5e7b323c6060500e702df73f
Riya Jain <riyaaajain286@gmail.com>
2026-02-23 10:21:54 +0530

    Time: 0 ms (100%), Space: 46.7 MB (81.01%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -4,24 +4,21 @@ class Solution {
         int m=image[0].length;
         int prevcolor=image[sr][sc];
         if(prevcolor==color) return image;
-        
         dfs(image,sr,sc,color,prevcolor);
         return image;
     }
-    
-    private void dfs(int[][] image,int i,int j,int color,int prevcolor){
-         image[i][j]=color;
-        int n=image.length;
-        int m=image[0].length;
-        int[] row={-1,0,1,0};
-        int[] col={0,1,0,-1};
-        for(int k=0;k<4;k++){
-            int nrow=i+row[k];
-            int ncol=j+col[k];
-            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&image[nrow][ncol]==prevcolor){
-             
-              dfs(image,nrow,ncol,color,prevcolor);
-            }
+    int[] delrow={-1,0,1,0};
+    int delcol[]={0,1,0,-1};
+    public void dfs(int[][] grid,int r,int c,int color,int prevcolor){
+        int n=grid.length;
+        int m=grid[0].length;
+        grid[r][c]=color;
+        for(int i=0;i<4;i++){
+             int nrow=r+delrow[i];
+             int ncol=c+delcol[i];
+             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&grid[nrow][ncol]==prevcolor){
+                dfs(grid,nrow,ncol,color,prevcolor);
+             }
         }
     }
 }
