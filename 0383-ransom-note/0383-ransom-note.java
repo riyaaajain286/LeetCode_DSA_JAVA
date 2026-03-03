3b3dfeb67a1547d314a6a1b79075453160ab98d5
Riya Jain <riyaaajain286@gmail.com>
2026-03-03 21:14:25 +0530

    Time: 1 ms (99.84%), Space: 46.2 MB (95.91%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int n=ransomNote.length();
+        int m=magazine.length();
+        if(n>m) return false;
+        int[] freq=new int[26];
+        for(int i=0;i<m;i++){
+          freq[magazine.charAt(i)-'a']++; 
+        }
+        for(int i=0;i<n;i++){
+          freq[ransomNote.charAt(i)-'a']--; 
+          if(freq[ransomNote.charAt(i)-'a']==-1) return false;
+        }
+        return true;
+    }
+}
