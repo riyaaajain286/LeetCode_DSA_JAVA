c956bbcb7ae8f16e093c58bef22918092dbe38d6
Riya Jain <riyaaajain286@gmail.com>
2026-03-06 00:04:27 +0530

    Time: 375 ms (84.83%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
-SELECT
--- p is short name for person table and a for address table
-    p.firstName,
-    p.lastName,
-    a.city,
-    a.state
+SELECT p.firstName,p.lastName,a.city,a.state
 FROM Person p
 LEFT JOIN Address a
 ON p.personId=a.personId;
