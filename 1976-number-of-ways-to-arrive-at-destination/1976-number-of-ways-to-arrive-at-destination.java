790eba2bd9df8c9ea95cc48698590cc61bdd29e3
Riya Jain <riyaaajain286@gmail.com>
2026-03-24 10:27:01 +0530

    Time: 12 ms (91.45%), Space: 52.2 MB (32.51%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.java
@@ -1,47 +1,52 @@
 class Solution {
     class Pair{
-        int dist,node;
-        public Pair(int dist,int node){
-            this.dist=dist;
-            this.node=node;
+        int node;
+        long dist;
+        public Pair(int n,long d){
+            this.node=n;
+            this.dist=d;
         }
     }
     public int countPaths(int n, int[][] roads) {
-        ArrayList<ArrayList<int[]>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int[] r:roads){
-            adj.get(r[0]).add(new int[]{r[1],r[2]});
-            adj.get(r[1]).add(new int[]{r[0],r[2]});
-        }
         int mod=(int)1e9+7;
-        long[] dist=new long[n];
-        long[] ways=new long[n];
-        Arrays.fill(dist,Long.MAX_VALUE);
-        dist[0]=0;
-        ways[0]=1;
-        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->(a.dist-b.dist));
-        pq.add(new Pair(0,0));
-        while(!pq.isEmpty()){
-            Pair p=pq.poll();
-            int u=p.node;
-            int d=p.dist;
-            if(d>dist[u])  continue;
-            for(int[] edge:adj.get(u)){
-                int v=edge[0];
-                int w=edge[1];
-            
-            if(dist[u]+w<dist[v]){
-                dist[v]=dist[u]+w;
-                ways[v]=ways[u];
-                pq.add(new Pair((int)dist[v],v));
-            }
-            else if(dist[u]+w==dist[v]){
+      ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
+      int V=n;
+       for(int i=0;i<V;i++){
+           adj.add(new ArrayList<>());
+       }
+       for(int[] e:roads){
+           adj.get(e[0]).add(new Pair(e[1],e[2]));
+           adj.get(e[1]).add(new Pair(e[0],e[2]));
+
+       }
+       long[] dist=new long[V];
+       //dp array
+       long[] ways=new long[V];
+       Arrays.fill(dist,Long.MAX_VALUE);
+       dist[0]=0;
+       ways[0]=1;//to reasch start node there is only 1 way
+       PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->Long.compare(a.dist,b.dist));//min heap
+       pq.add(new Pair(0,0));
+       while(!pq.isEmpty()){
+           Pair p=pq.poll();
+           int u=p.node;
+           long d=p.dist;
+           // Skip outdated entries
+               if(d>dist[u]) continue;
+           for(Pair adjnode:adj.get(u)){
+               int v=adjnode.node;
+               long w=adjnode.dist;
+               
+               if(d+w<dist[v]){
+                   dist[v]=d+w;
+                   ways[v]=ways[u];
+                   pq.add(new Pair(v,dist[v]));
+               }
+               else if(d+w==dist[v]){
                 ways[v]=(ways[v]+ways[u])%mod;
-            }
-        }
-        }
-        return (int)ways[n-1]%mod;
+               }
+           }
+       }
+       return (int)ways[n-1]%mod;
     }
 }
