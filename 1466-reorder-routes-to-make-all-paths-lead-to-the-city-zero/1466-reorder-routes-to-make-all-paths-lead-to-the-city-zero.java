dd44c6caaf4f7d34d416e78140e6a81e491bed7a
Riya Jain <riyaaajain286@gmail.com>
2026-03-26 13:07:26 +0530

    Time: 42 ms (61.89%), Space: 118.2 MB (50.51%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,36 @@
+class Solution {
+   
+    class Pair{
+        int node,check;
+        public Pair(int n,int c){
+            this.node=n;
+            this.check=c;
+        }
+    }
+    public int minReorder(int n, int[][] connections) {
+        ArrayList<ArrayList<Pair>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] c:connections){
+            adj.get(c[0]).add(new Pair(c[1],1));
+             adj.get(c[1]).add(new Pair(c[0],0));
+        }
+        boolean[] vis=new boolean[n];
+        int ans=dfs(adj,vis,0);
+        return ans;
+    }
+    private int dfs(ArrayList<ArrayList<Pair>> adj,boolean[] vis,int u){
+        vis[u]=true;
+         int count=0;
+        for(Pair v:adj.get(u)){
+            int node=v.node;
+            int check=v.check;
+            if(!vis[node]){
+                count+=check;
+                count+=dfs(adj,vis,node);
+            }
+        }
+        return count;
+    }
+}
