8d2958908ba405131dd6e2913b341dcb49083943
Riya Jain <riyaaajain286@gmail.com>
2026-04-04 20:33:17 +0530

    Time: 24 ms (42.77%), Space: 54.3 MB (18.49%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,56 +1,57 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
-      String ans=lcs(str1,str2);
-      return ans;
+        return lcsB(str1,str2);
     }
-    public String lcs(String str1,String str2){
-        //build lcs dp table
-        int n=str1.length();
-        int m=str2.length();
-        int[][] dp=new int[n+1][m+1];
-        //initialization
-        // if(n==0||m==0) return 0;
-        //iteration
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(str1.charAt(i-1)==str2.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
-                }
+    public static String lcsB(String text1, String text2){
+        int n=text1.length();
+        int m=text2.length();
+       int[][] dp=new int[n+1][m+1];
+       for(int i=0;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            if(i==0||j==0){
+                dp[i][j]=0;
             }
         }
-       //bactrack to find lcs string
-       int i=n,j=m;//initialized with last index so backtrack from last cell
-       StringBuilder sb=new StringBuilder();
-       while(i>0&&j>0){
-        // Case 1: Characters match → part of LCS
-        if(str1.charAt(i-1)==str2.charAt(j-1)){//matching characters
-            sb.append(str1.charAt(i-1));
-            i--;
-            j--;
+    }
+     //iteration
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+         if(text1.charAt(i-1)==text2.charAt(j-1)){
+            dp[i][j]= 1+dp[i-1][j-1];
         }
-        // Case 2: Move up → take character from s1
-        else if(dp[i-1][j]>dp[i][j-1]){//comparing max value in tabble across diagonal
-            sb.append(str1.charAt(i-1));
-            i--;
-        }
-        // Case 3: Move left → take character from s2
         else{
-            sb.append(str2.charAt(j-1));
+            dp[i][j]= Math.max(dp[i][j-1],dp[i-1][j]);
+        }
+          }
+       }
+       int i=n,j=m;
+       StringBuilder sb=new StringBuilder();
+       while(i>0 && j>0){
+        if(text1.charAt(i-1)==text2.charAt(j-1)){
+            sb.append(text1.charAt(i-1));
+            i--;
             j--;
         }
-       }
-       //add remaining characters
-       while(i>0){
-         sb.append(str1.charAt(i-1));
+        else if(dp[i-1][j]>dp[i][j-1]){
+            sb.append(text1.charAt(i-1));
             i--;
-       }
-       while(j>0){
-        sb.append(str2.charAt(j-1));
+        }
+        else{
+            sb.append(text2.charAt(j-1));
             j--;
-       }
-      return sb.reverse().toString();
+        }
+    }
+    //add remaining
+    while(i>0){
+        sb.append(text1.charAt(i-1));
+        i--;
+    }
+     while(j>0){
+        sb.append(text2.charAt(j-1));
+        j--;
+    }
+    sb.reverse();
+    String rev=sb.toString();
+    return rev;
     }
 }
