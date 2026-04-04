f1f773545aa66f60cf76734028dbe4abc2652436
Riya Jain <riyaaajain286@gmail.com>
2026-04-04 19:42:18 +0530

    Time: 28 ms (40.89%), Space: 47.8 MB (14.61%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,23 +1,33 @@
 class Solution {
     public int minInsertions(String s) {
-       StringBuilder sb=new StringBuilder(s);
-       String rev=sb.reverse().toString(); 
-       int ans=s.length()-lps(s,rev);
-       return ans;
-    }
-    public int lps(String s,String rev){
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        String rev=sb.toString();
         int n=s.length();
-        int[][] dp=new int[n+1][n+1];
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(s.charAt(i-1)==rev.charAt(j-1)){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
-                }
+        int lcs=lcs(s,rev);
+        return n-lcs;
+    }
+    private int lcs(String s,String rev){
+        int n=s.length();
+        int m=rev.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+          for(int j=0;j<=m;j++){
+            if(i==0||j==0){
+            dp[i][j]=0;
             }
+          }
         }
-        return dp[n][n];
+        for(int i=1;i<=n;i++){
+          for( int j=1;j<=m;j++){
+            if(s.charAt(i-1)==rev.charAt(j-1)){
+            dp[i][j]=1+dp[i-1][j-1];
+            }
+            else {
+            dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+            }
+         }
+        }
+        return dp[n][m];
     }
 }
