759807a94dd539d4a429904b8978cfa110fa85c9
Riya Jain <riyaaajain286@gmail.com>
2026-04-05 12:13:39 +0530

    Time: 1 ms (41.32%), Space: 46.7 MB (84.45%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,24 +1,25 @@
 class Solution {
+    int[] dr={-1,0,1,0};
+    int[] dc={0,1,0,-1};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n=image.length;
         int m=image[0].length;
-        int prevcolor=image[sr][sc];
-        if(prevcolor==color) return image;
-        dfs(image,sr,sc,color,prevcolor);
-        return image;
+        int prevc=image[sr][sc];
+        if(prevc==color) return image;
+         dfs(image,sr,sc,color,prevc);
+           return image;
+        
     }
-    int[] delrow={-1,0,1,0};
-    int delcol[]={0,1,0,-1};
-    public void dfs(int[][] grid,int r,int c,int color,int prevcolor){
-        int n=grid.length;
-        int m=grid[0].length;
-        grid[r][c]=color;
-        for(int i=0;i<4;i++){
-             int nrow=r+delrow[i];
-             int ncol=c+delcol[i];
-             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&grid[nrow][ncol]==prevcolor){
-                dfs(grid,nrow,ncol,color,prevcolor);
-             }
+    public void dfs(int[][] image,int r,int c,int color,int prevc){
+        image[r][c]=color;
+        int n=image.length;
+        int m=image[0].length;
+        for(int k=0;k<4;k++){
+            int nr=r+dr[k];
+            int nc=c+dc[k];
+            if(nr>=0 && nc>=0 && nr<n && nc<m && image[nr][nc]==prevc){
+                dfs(image,nr,nc,color,prevc);
+            }
         }
     }
 }
