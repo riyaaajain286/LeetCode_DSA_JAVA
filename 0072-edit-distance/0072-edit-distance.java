8229a350e5a6ecdd1e4b2c148922cf4525666ed7
Riya Jain <riyaaajain286@gmail.com>
2026-04-07 00:30:56 +0530

    Time: 3 ms (98.35%), Space: 47.1 MB (76.38%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,28 +1,29 @@
 class Solution {
-    //tabulation
     int[][] dp;
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
         dp=new int[n+1][m+1];
-        //initialization
-        for(int i=0;i<=n;i++) 
-         dp[i][0]=i;
-        for(int j=0;j<=m;j++)
-          dp[0][j]=j;
-        //iteration
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-               if(word1.charAt(i-1)==word2.charAt(j-1)) 
-                 dp[i][j]=dp[i-1][j-1];
-        else{
-         dp[i][j]=1+Math.min(Math.min(dp[i][j-1], //insert
-            dp[i-1][j]),//delete
-            dp[i-1][j-1]);//replace
+        for(int[] r:dp){
+            Arrays.fill(r,-1);
         }
-       }
-      }
-     return dp[n][m];
+        return solve(word1,word2,n-1,m-1);
+    }
+    private int solve(String word1, String word2,int i,int j){
+        int n=word1.length();
+        int m=word2.length();
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+         if(dp[i][j]!=-1) return dp[i][j];
+        if(word1.charAt(i)==word2.charAt(j)){
+            dp[i][j]= 0+solve(word1,word2,i-1,j-1);
+        }else{
+          int insert= 1+solve(word1,word2,i,j-1);
+          int delete= 1+solve(word1,word2,i-1,j);
+          int replace= 1+solve(word1,word2,i-1,j-1);
+            dp[i][j]=Math.min(insert,(Math.min(delete,replace)));
         }
+          return dp[i][j];
+        
+    }
 }
- 
