463aa05c214ad59be4022b280995b287fd0f9f16
Riya Jain <riyaaajain286@gmail.com>
2026-04-11 11:56:21 +0530

    Time: 1 ms (98.09%), Space: 42.4 MB (83.87%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,13 +1,7 @@
 class Solution {
+    //by digital root formula
     public int addDigits(int num) {
-        while(num>=10){
-            int sum=0;
-            while(num>0){
-                sum+=num%10;
-                num/=10;
-            }
-            num=sum;
-        }
-        return num;
+        if(num==0) return 0;
+        return 1+(num-1)%9;
     }
 }
