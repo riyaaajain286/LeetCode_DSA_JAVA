14ede6f84d98098e38e133870de18f9c56037fa3
Riya Jain <riyaaajain286@gmail.com>
2026-04-19 18:11:04 +0530

    Time: 1 ms (13.54%), Space: 42.2 MB (32.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,18 +1,22 @@
 class Solution {
-    //memoization
+    //tabulation
     int[][] dp;
     public int uniquePaths(int m, int n) {
         dp=new int[m][n];
         for(int[] r:dp)
            Arrays.fill(r,-1);
-        return helper(m,n,m-1,n-1);
+        for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+            if(i==0 && j==0) dp[i][j]=1;
+            else{
+                int up=0,left=0;
+                if(i>0)  up=dp[i-1][j];
+                if(j>0)  left=dp[i][j-1];
+                dp[i][j]=up+left;
+            }
+           }
+        }
+        return dp[m-1][n-1];
     }
-    public int helper(int m, int n,int i,int j){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=helper(m,n,i-1,j);
-        int left=helper(m,n,i,j-1);
-        return dp[i][j]=up+left;
-    }
+   
 }
