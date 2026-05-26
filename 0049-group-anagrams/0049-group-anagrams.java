2bf0a7f8f8d26ea1815315e5c3f40a791c9a4a1c
Riya Jain <riyaaajain286@gmail.com>
2026-05-27 00:27:23 +0530

    Time: 20 ms (14.11%), Space: 50 MB (23.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -3,8 +3,17 @@ class Solution {
         HashMap<String,List<String>> map=new HashMap<>();
         for(String s:strs){
             char[] ch=s.toCharArray();
-            Arrays.sort(ch);
-            String key=new String(ch);
+            // Arrays.sort(ch);
+            int[] freq=new int[26];
+            //freq reduces sorting complexity now n*k instead if n*klog k
+            for(char c:ch){
+                freq[c-'a']++;
+            }
+            StringBuilder sb=new StringBuilder();
+            for(int i:freq){
+              sb.append(i).append("#");
+            }
+            String key=sb.toString();
             if(!map.containsKey(key)){
                 map.put(key,new ArrayList<>());
             }
