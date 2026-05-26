8ec72154c22426b3e5d6991debf5f73d06157efc
Riya Jain <riyaaajain286@gmail.com>
2026-05-26 13:08:16 +0530

    Time: 13 ms (29.77%), Space: 46.4 MB (40.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,12 +5,10 @@ class Solution {
         if(n!=m) return false;
         HashMap<Character,Integer> map1=new HashMap<>();
         HashMap<Character,Integer> map2=new HashMap<>();
-        char[] a=s.toCharArray();
-        char[] b=t.toCharArray();
-        for(char i:a){
+        for(char i:s.toCharArray()){
             map1.put(i,map1.getOrDefault(i,0)+1);
         }
-        for(char j:b){
+        for(char j:t.toCharArray()){
             map2.put(j,map2.getOrDefault(j,0)+1);
         }
         return map1.equals(map2);
