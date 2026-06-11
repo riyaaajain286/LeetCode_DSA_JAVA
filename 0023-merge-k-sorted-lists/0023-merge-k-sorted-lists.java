531aa5e3a3ebd2335e4dcc80c48eb88071c9f311
Riya Jain <riyaaajain286@gmail.com>
2026-06-11 11:09:06 +0530

    Time: 4 ms (81.91%), Space: 46.9 MB (45.61%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,29 +9,24 @@
  * }
  */
 class Solution {
+    //using priority queue
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists==null||lists.length==0){
-    return null;
-   }
-   //minheap to store k heads of each lists and gives smallest
-   PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->a.val-b.val);
-   //add first node of each lists to heap ie address
-   for(ListNode node:lists){
-    if(node!=null){
-      pq.add(node);
-    }
-   }
-   //dummy node to built result
-   ListNode dummy=new ListNode(0);
-   ListNode tail=dummy;
-   while(!pq.isEmpty()){
-    ListNode minnode=pq.poll();
-    tail.next=minnode;
-    tail=tail.next;
-   if(minnode.next!=null){
-    pq.add(minnode.next);
-   }
-   }
-   return dummy.next;
+        if(lists==null || lists.length==0) return null;
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->a.val-b.val);
+        for(ListNode node:lists){
+            if(node!=null)
+              pq.add(node);
+        }
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+        while(!pq.isEmpty()){
+          ListNode minnode=pq.poll();
+          temp.next=minnode;
+          temp=temp.next;
+          if(minnode.next!=null){
+            pq.add(minnode.next);
+          }
+        }
+        return dummy.next;
     }
 }
