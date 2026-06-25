933e11a4d4fdc05d7138b6dc7f2ef4a9ff46ed63
Riya Jain <riyaaajain286@gmail.com>
2026-06-25 11:59:38 +0530

    Time: 8 ms (67.71%), Space: 45.7 MB (97.63%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -8,12 +8,13 @@ class Solution {
            maxfreq=Math.max(maxfreq,freq[s.charAt(r)-'A']);
            while((r-l+1)-maxfreq>k){
             freq[s.charAt(l)-'A']--;
-            // maxfreq=0;
+            maxfreq=0;
             l++;
+           
+           for(int i=0;i<26;i++){
+            maxfreq=Math.max(maxfreq,freq[i]);
            }
-        //    for(int i=0;i<26;i++){
-        //     maxfreq=Math.max(maxfreq,freq[i]);
-        //    }
+           }
            if((r-l+1)-maxfreq<=k){
             maxlen=Math.max(maxlen,(r-l+1));
             
