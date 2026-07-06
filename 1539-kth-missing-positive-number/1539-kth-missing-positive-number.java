ecd388c1e9fa232f9911f3dbbf39545faf44b20a
Riya Jain <riyaaajain286@gmail.com>
2026-07-06 14:54:30 +0530

    Time: 0 ms (100%), Space: 44.7 MB (39.97%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,5 +1,7 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
+        // This means if the array did not exist,the positive no should simply be that no 
+        // But every array element <= k is a number that is present, so it pushes the k-th missing number one position forward.
         for(int n:arr){
             if(n<=k){
                 k++;
