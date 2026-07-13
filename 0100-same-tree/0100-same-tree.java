d560980dc47ad4aeebf700ca19df9c3999b264f5
Riya Jain <riyaaajain286@gmail.com>
2026-07-13 12:08:51 +0530

    Time: 0 ms (100%), Space: 43 MB (33.44%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,8 +18,9 @@ class Solution {
         if(p==null && q==null) return true;
         if(p==null || q==null) return false;
         if(p.val!=q.val) return false;
-        boolean left=isSameTree(p.left,q.left);
-        boolean right=isSameTree(p.right,q.right);
-        return left&&right;
+        if(isSameTree(p.left,q.left) &&  isSameTree(p.right,q.right)){
+            return true;
+        }
+        return false;
     }
 }
