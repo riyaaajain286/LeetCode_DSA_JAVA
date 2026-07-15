3701bd99a01f2bb4e7b525e5e2fe830d48fcb435
Riya Jain <riyaaajain286@gmail.com>
2026-07-15 13:10:47 +0530

    Time: 1 ms (60.6%), Space: 47.6 MB (81.78%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,21 @@
 class Solution {
     //2 pointer approach
     public int trap(int[] height) {
-        int n=height.length;
-        int l=0;
-        int r=n-1;
-        int lmax=0,rmax=0;
-        int ans=0;
-        while(l<r){
-            lmax=Math.max(lmax,height[l]);
-            rmax=Math.max(rmax,height[r]);
-            if(lmax<rmax){
-                ans+=lmax-height[l];
-                l++;
-            }
-            else{
-                ans+=rmax-height[r];
-                r--;
-            }
+       int lmax=0,rmax=0;
+       int l=0,r=height.length-1;
+       int totalWater=0;
+       while(l<r){
+        lmax=Math.max(lmax,height[l]);
+        rmax=Math.max(rmax,height[r]);
+        if(lmax<rmax){
+          totalWater+=lmax-height[l];
+          l++;
         }
-        return ans;
+        else{
+          totalWater+=rmax-height[r];
+          r--;
+        }
+       }
+        return totalWater;
     }
 }
