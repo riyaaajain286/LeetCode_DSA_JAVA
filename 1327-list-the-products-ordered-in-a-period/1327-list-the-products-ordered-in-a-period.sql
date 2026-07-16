63942a54adc61eeb4ed6fcd3be7274dc21af2c4c
Riya Jain <riyaaajain286@gmail.com>
2026-07-16 13:31:06 +0530

    Time: 1002 ms (7.86%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -4,7 +4,7 @@ SUM(o.unit) AS unit
 FROM Products p
 INNER JOIN Orders o
 ON p.product_id=o.product_id
-WHERE YEAR(order_date)='2020' AND MONTH(order_date)='02'
--- WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-28'
+-- WHERE YEAR(order_date)='2020' AND MONTH(order_date)='02'
+WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
 GROUP BY p.product_id
 HAVING SUM(o.unit)>=100;
