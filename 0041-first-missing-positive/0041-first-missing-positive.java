1845b70210fa7a2c5462a078ad6be434b18ea06f
Riya Jain <riyaaajain286@gmail.com>
2026-07-18 19:44:00 +0530

    Time: 13 ms (30.9%), Space: 93.4 MB (6.24%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,35 +1,14 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int len=nums.length;
-        // Check if 1 is present
-       boolean containsOne=false;
-       for(int n:nums){
-        if(n==1){
-            containsOne=true;
-            break;
+        HashSet<Integer> set=new HashSet<>();
+        for(int n:nums){
+            if(n>0)
+             set.add(n);
         }
-       } 
-       if(!containsOne) return 1;
-       //Replace invalid values with 1
-       for(int i = 0; i < len; i++){
-         if(nums[i] <= 0 || nums[i] > len){
-           nums[i] = 1;
-         }  
-       }
-       //Mark indices
-       for(int j=0;j<len;j++){
-        int num=Math.abs(nums[j]);
-        int index=num-1;
-        if(nums[index]>0){
-            nums[index]=-nums[index];
+        int ans=1;
+        while(set.contains(ans)){
+            ans++;
         }
-       }
-    //    Find first positive
-       for(int k=0;k<len;k++){
-        if(nums[k]>0){
-            return k+1;
-        }
-       }
-       return len+1;
+        return ans;
     }
 }
