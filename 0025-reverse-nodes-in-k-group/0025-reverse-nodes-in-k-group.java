be6f52126fb5f874d0ea034efba42540b7597e21
Riya Jain <riyaaajain286@gmail.com>
2026-07-23 13:12:34 +0530

    Time: 0 ms (100%), Space: 46.4 MB (35.32%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.java
@@ -10,40 +10,31 @@
  */
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
+      ListNode prevnode=null;
         ListNode temp=head;
-        ListNode prevnode=null;
         while(temp!=null){
-            ListNode kthNode=knode(temp,k);
-            if(kthNode==null){
-                if(prevnode!=null){
-                    prevnode.next=temp;
-                }
-                break;
-            }
-            ListNode nextnode=kthNode.next;
-            kthNode.next=null;
-            reverse(temp);
-            //while reversing 1st set temp which is kth node will become head of reversed ll therefore make it as newhead now
-            // First group
-            if(temp==head)
-              head=kthNode;
-            //connecting reversed list with last set of list
-            else{
-              prevnode.next=kthNode;
-            }
-            prevnode=temp;
-            temp=nextnode;
+        ListNode knode=kthNode(temp,k);
+        if(knode==null){
+            if(prevnode!=null)
+              prevnode.next=temp;
+            break;
+        }
+        ListNode nextnode=knode.next;
+        knode.next=null;
+        reverse(temp);
+
+        if(temp==head){
+            head=knode;
+        }
+        else{
+        prevnode.next=knode;
+        }
+        
+        prevnode=temp;
+        temp=nextnode;
         }
         return head;
-    }
-    private ListNode knode(ListNode head,int k){
-        k-=1;
-        ListNode temp=head;
-        while(temp!=null && k>0){
-            k--;
-            temp=temp.next;
-        }
-        return temp;
+
     }
     private ListNode reverse(ListNode head){
         if(head==null || head.next==null){
@@ -58,4 +49,14 @@ class Solution {
         }
         return prev;
     }
+    private ListNode kthNode(ListNode head,int k){
+        k=k-1;
+        ListNode temp=head;
+        while(temp!=null && k>0){
+            k--;
+            temp=temp.next;
+        }
+        return temp;
+    }  
+    
 }
