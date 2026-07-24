5b1bb8918c6bc599b983aeba4f6a058d7f91266a
Riya Jain <riyaaajain286@gmail.com>
2026-07-24 11:38:57 +0530

    Time: 2 ms (98.24%), Space: 43.2 MB (57.96%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,29 +2,26 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<>();
         for(char ch:s.toCharArray()){
-            // if(ch=='(' || ch=='[') ||ch=='{'){
-            if(ch=='('||ch=='{'||ch=='['){
-                st.push(ch);
-            }
-            else{
-                if(ch==')'){
-                    if(st.isEmpty() || st.peek()!='('){
-                        return false;
-                    }
+            if(ch=='(' || ch=='{' || ch=='['){
+                    st.push(ch);
+             }
+             else{
+                if(st.isEmpty()) return false;
+
+                char top=st.pop();
+
+                if( ch==')' && top!='('){
+                 return false;
                 }
-                  if(ch==']'){
-                    if(st.isEmpty() || st.peek()!='['){
-                        return false;
-                    }
+                if(ch==']' && top!='['){
+                return false;
                 }
-                  if(ch=='}'){
-                    if(st.isEmpty() || st.peek()!='{'){
-                        return false;
-                    }
+                if(ch=='}' && top!='{'){
+                return false;
                 }
-                st.pop();
+                
             }
         }
-       return st.isEmpty();
+        return st.isEmpty();
     }
 }
