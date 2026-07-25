9a6cc24d94596d62a339bb284b4741845cb82984
Riya Jain <riyaaajain286@gmail.com>
2026-07-25 11:58:29 +0530

    Time: 15 ms (82.24%), Space: 46.8 MB (34.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,31 +1,33 @@
 class Solution {
+    // BOTTOM UP
     int[][] dp;
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
         dp=new int[n+1][amount+1];
-        for(int[] r:dp){
-            Arrays.fill(r,-1);
-        }
-        int ans= unbounded(coins,amount,n);
         
-        if(ans==Integer.MAX_VALUE-1) return -1;
-        else return ans;
+        int ans= unbounded(coins,amount,n);
+        return ans;
+        // if(ans==Integer.MAX_VALUE-1) return -1;
+        // else return ans;
     }
     private int unbounded(int[] coins,int amount,int n){
-       if(amount==0 ){
-        return 0;
-       }
-       if(n==0) return Integer.MAX_VALUE-1;
-
-       if(dp[n][amount]!=-1) return dp[n][amount];
-       if(coins[n-1]<=amount){
-        dp[n][amount]= Math.min(1+unbounded(coins,amount-coins[n-1],n),
-        unbounded(coins,amount,n-1));
-       }
-       else{
-        dp[n][amount]= unbounded(coins,amount,n-1);
-       }
-       return dp[n][amount];
+      for(int  j=1;j<=amount;j++) 
+        dp[0][j]=Integer.MAX_VALUE-1;
+      for(int i=0;i<=n;i++) 
+         dp[i][0]=0;
+        
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=amount;j++){
+            if(coins[i-1]<=j){
+                dp[i][j]=Math.min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+            }
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+     }
+       
+       return dp[n][amount]==Integer.MAX_VALUE-1?-1:dp[n][amount];
     }  
 
     
