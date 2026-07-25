37b7de0c0666a66fbd81cb88be98b8dd87ed6ac2
Riya Jain <riyaaajain286@gmail.com>
2026-07-25 10:55:11 +0530

    Time: 12 ms (48.79%), Space: 56.2 MB (55.33%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,24 +1,28 @@
 class Solution {
-    //memoization
+    //BOTTOM UP
     int[][] dp;
     public int change(int amount, int[] coins) {
        int n=coins.length; 
        dp=new int[n+1][amount+1];
-       for(int[] r:dp){
-           Arrays.fill(r,-1);
-       }
-       return unboundedKnapsack(coins,amount,n);
-    }
-    private int unboundedKnapsack(int[] coins,int amt,int n){
-        if(n==0) return 0;
-        if(amt==0) return 1;
-        if(dp[n][amt]!=-1) return dp[n][amt];
-        if(coins[n-1]<=amt){
-            dp[n][amt]= (unboundedKnapsack(coins,amt-coins[n-1],n)+unboundedKnapsack(coins,amt,n-1));
+     
+        //initialization
+        for(int i=1;i<=n;i++){
+            dp[i][0]=1;
         }
-        else{
-           dp[n][amt] =unboundedKnapsack(coins,amt,n-1);
+        for(int j=0;j<=amount;j++){
+           dp[0][j]=0;
         }
-        return dp[n][amt];
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j] =dp[i-1][j];
+                }
+            }
+        }
+       
+        return dp[n][amount];
     }
 }
