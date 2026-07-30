08a2d849f9af684c748d99ee3cbb6c7edbf12033
Riya Jain <riyaaajain286@gmail.com>
2026-07-30 23:58:52 +0530

    Time: 6 ms (72.49%), Space: 45.2 MB (36.55%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,45 +1,31 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-       int sum=0;
-    int zerocount=0;
-    for(int n:nums){
-      sum+=n;
-      if(n==0) zerocount++;
-    }
-    // ❗ absolute target cannot exceed sum
-    if(Math.abs(target)>sum) return 0;
-    //sum +differnece must be even
-    if((sum+target)%2!=0) return 0;
-    int s1=(target+sum)/2;
-    // ❗ s1 must be non-negative
-    if(s1<0) return 0;
-    int ways= subsetCount(nums,s1);
-    //for handling zero elements
-    return ways*(int)(Math.pow(2,zerocount));
+        int sum=0;
+        for(int n:nums){
+        sum+=n;
+        }
+        if(Math.abs(target)>sum) return 0;
+        if((sum+target)%2!=0) return 0;
+        int s1=(sum+target)/2;
+        int count=count(nums, s1);
+        return count;
+        }
+  private static int  count(int[] nums,int target){
+      int n=nums.length;
+      int[][] dp=new int[n+1][target+1];
+      //initialization
+      dp[0][0]=1;
+      for(int i=1;i<=n;i++){
+        for(int j=0;j<=target;j++){
+          if(nums[i-1]<=j){
+            dp[i][j]=dp[i-1][j-nums[i-1]]+dp[i-1][j];
+        }
+        else{
+          dp[i][j]=dp[i-1][j];
+        }
+      }
+      }
+      return dp[n][target];
   } 
-  public static int subsetCount(int[] arr,int sum){
-    int n=arr.length;
-    int[][] dp=new int[n+1][sum+1];
-    for(int i=0;i<=n;i++){
-      dp[i][0]=1;
-    }
-    for(int j=1;j<=sum;j++){
-      dp[0][j]=0;
-    }
-    for(int i=1;i<=n;i++){
-      for(int j=1;j<=sum;j++){
-         if(arr[i-1]==0){
-              dp[i][j]=dp[i-1][j];//zero handled later
-          }
-        else if(arr[i-1]<=j){
-        dp[i][j]=dp[i-1][j-arr[i-1]]+dp[i-1][j];
-      }
-      else{
-        dp[i][j]=dp[i-1][j];
-      }
-       
-    }
-  }
-  return dp[n][sum];
-    }
+    
 }
