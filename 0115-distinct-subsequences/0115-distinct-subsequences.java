681be1292f52306e0899f3b8d3ca3ea8d8ca33a0
Riya Jain <riyaaajain286@gmail.com>
2026-08-03 15:40:20 +0530

    Time: 20 ms (53.42%), Space: 57.9 MB (14.82%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,32 +1,25 @@
 class Solution {
-    //tabulation
-    int[][] dp;
+    static int[][] dp;
     public int numDistinct(String s, String t) {
-        int n=s.length();
+       int n=s.length();
         int m=t.length();
         dp=new int[n+1][m+1];
-       for(int i=0;i<n;i++){
-        dp[i][0]=1;//if pattern string exhauts meaning matched therefore return 1
-       }
-     
-        // if(i<0) return 0;//if original string exhauts meaning not matched with pattern and it is finished so return 0
-        
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                int op1=0;
-                int op2=0;
-                if(s.charAt(i-1)==t.charAt(j-1)){
-                    op1=dp[i-1][j-1]+dp[i-1][j];
-                    }
-                else{
-                   op2=dp[i-1][j];
-                }
-                dp[i][j]=op1+op2;
-            }
-        }
-       
-    
-     return dp[n][m];
+        for(int[] r:dp)
+         Arrays.fill(r,-1);
+        return solve(s,t,n-1,m-1);
+    }
+    private static  int solve(String s, String t,int i,int j){
+      int n=s.length();
+        int m=t.length();
+      if(j<0) return 1;
+      if(i<0) return 0;
+      if(dp[i][j]!=-1) return dp[i][j];
+      if(s.charAt(i)==t.charAt(j)){
+        dp[i][j]=solve(s,t,i-1,j-1) + solve(s,t,i-1,j);
+      }
+      else{
+        dp[i][j]=solve(s,t,i-1,j);
+      }
+      return dp[i][j]; 
     }
 }
