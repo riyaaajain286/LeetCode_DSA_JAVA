4f2c4eb5aca815ea08d54f642f3442e9dfe83e8c
Riya Jain <riyaaajain286@gmail.com>
2026-08-04 15:53:27 +0530

    Time: 73 ms (11.8%), Space: 50.5 MB (7.26%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.java
+++ 0087-scramble-string/0087-scramble-string.java
@@ -1,32 +1,42 @@
 class Solution {
-    Map<String,Boolean> memo=new HashMap<>(); 
+    static HashMap<String,Boolean> map=new HashMap<>();
     public boolean isScramble(String s1, String s2) {
-        return solve(s1,s2);
+         return solve(s1,s2); 
+   }
+   private static boolean solve(String a,String b){
+    if(a.equals(b)) return true;
+    int n=a.length(),m=b.length();
+    if(n!=m) return false;
+    if(n==1){
+      return a.equals(b);
     }
-    public boolean solve(String a,String b){
-        int n=a.length();
-        //base condn
-        if(a.equals(b)) return true;
-        if(a.length()!=b.length()) return false;
-        if(n==1) 
-         return a.equals(b);
-         String key=a+" "+b;
-         if(memo.containsKey(key))
-           return memo.get(key);
-        //k loop
-        for(int i=1;i<=n-1;i++){
-            //swap case
-            if(solve(a.substring(0,i),b.substring(n-i)) && solve(a.substring(i,n),b.substring(0,n-i))){
-                memo.put(key,true);
-                return true;
-            }
-            //no swap case
-            if(solve(a.substring(0,i),b.substring(0,i)) && solve(a.substring(i,n),b.substring(i,n))){
-                memo.put(key,true);
-                return true;
-            }
-        }
-        memo.put(key,false);
-        return false;
+    String key=a+" "+b;
+    if(map.containsKey(key)) return map.get(key);
+    boolean flag=false;
+    for(int k=1;k<=n-1;k++){
+      //swap case
+      boolean condition1=solve(a.substring(0,k),b.substring(n-k,n))
+                         &&
+                         solve(a.substring(k,n),b.substring(0,n-k));
+        
+        // return true;
+      
+
+      //no swap case
+      boolean condition2=solve(a.substring(0,k),b.substring(0,k))
+                         &&
+                         solve(a.substring(k,n),b.substring(k,n));
+     
+        // return true;
+       
+       if(condition1 || condition2){
+        flag=true;
+        // map.put(key,flag);
+        break;
+       }
     }
+    map.put(key,flag);
+     return flag;
+   
+   }
 }
