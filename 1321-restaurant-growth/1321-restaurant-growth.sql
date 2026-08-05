1a145ac3f4194b7cc3feedb5da370b4486e4b37e
Riya Jain <riyaaajain286@gmail.com>
2026-08-05 12:01:57 +0530

    Time: 474 ms (11.04%), Space: 0B (100%) - LeetHub

--- 1321-restaurant-growth/1321-restaurant-growth.sql
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -1,20 +1,17 @@
 # Write your MySQL query statement below
 SELECT visited_on,
-sum(amount) OVER(
-ORDER BY  visited_on
-ROWS  BETWEEN 6 PRECEDING AND CURRENT ROW
-) AS amount,
-ROUND(
-    AVG(amount) OVER(
-        ORDER BY visited_on
-        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
-    ),2
- ) AS average_amount
-
-FROM(
-    SELECT visited_on,
-    SUM(amount) AS amount 
-    FROM Customer
-    GROUP BY visited_on
-) x
+SUM(amount) OVER(
+    ORDER BY visited_on
+    ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)as amount,
+ROUND(AVG(amount) OVER(
+    ORDER BY visited_on
+    ROWS BETWEEN 6 PRECEDING AND CURRENT ROW),2)
+as average_amount
+-- The subquery is used because one date can have multiple customers/rows, but the window function should work on one total amount per day.
+FROM (
+SELECT visited_on,
+SUM(amount) AS amount
+FROM Customer
+GROUP BY visited_on
+) AS x
 LIMIT 100 OFFSET 6;
