1deec9782e9a70ab6249361ddeb09751db71f073
Riya Jain <riyaaajain286@gmail.com>
2026-08-07 19:09:59 +0530

    Time: 401 ms (5.13%), Space: 45.5 MB (95.12%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,26 +1,22 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
         int n=nums.length;
-       if(n<4) return ans;
-       Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            if(i>0&&nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n;j++){
-                if(j>i+1&&nums[j]==nums[j-1]) continue;
-                Set<Long> set=new HashSet<>();
-                 for(int k=j+1;k<n;k++){
-                    long sum=(long)nums[i]+(long)nums[j]+(long)nums[k];
-                    long fourth=(long)target-sum;
-                    if(set.contains(fourth)){
-                       List<Integer> quad=Arrays.asList(nums[i],nums[j],nums[k],(int)fourth);
-                       Collections.sort(quad);
-                       if(!ans.contains(quad)){
-                        ans.add(quad);
-                       }
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<n-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                for(int k=j+1;k<n-1;k++){
+                    if(k>j+1 && nums[k]==nums[k-1]) continue;
+                    for(int l=k+1;l<n;l++){
+                        if(l>k+1 && nums[l]==nums[l-1]) continue;
+                        long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
+                        if(sum==target){
+                          ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        }
                     }
-                         set.add((long)nums[k]);
-                 }
+                }
             }
         }
         return ans;
