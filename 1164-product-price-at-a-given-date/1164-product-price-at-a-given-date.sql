f51960037c1e68d11b775d65e46967334c71f9af
Riya Jain <riyaaajain286@gmail.com>
2026-08-12 22:15:09 +0530

    Time: 867 ms (11.39%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -7,16 +7,14 @@ FROM Products) r
 LEFT JOIN
 (
     SELECT product_id,
-    new_price
+    new_price,
+    row_number() OVER(
+    PARTITION BY product_id
+    ORDER BY change_date DESC
+    ) AS rn
     FROM Products
-    WHERE(product_id,change_date) in
-    (
-        SELECT product_id,
-        MAX(change_date) AS latest_date
-        FROM Products 
-        WHERE change_date<='2019-08-16'
-        GROUP BY product_id
-    )
+    WHERE change_date<='2019-08-16'
 )t
-ON r.product_id=t.product_id;
+ON r.product_id=t.product_id
+AND t.rn=1;
 
