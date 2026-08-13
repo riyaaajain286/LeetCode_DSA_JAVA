a4718944f5c43e6ef05d9f95f51d0c8f05262c3e
Riya Jain <riyaaajain286@gmail.com>
2026-08-13 23:54:49 +0530

    Time: 4 ms (45.9%), Space: 52.4 MB (46.26%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,33 +2,32 @@ class Solution {
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
-        boolean vis[][]=new boolean[n][m];
-        int island=0;
+        boolean[][] vis=new boolean[n][m];
+        int c=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                   if(!vis[i][j]){
-                   
-                    dfs(grid,vis,i,j);
-                     island++;
-                   }
-                }
+               if(grid[i][j]=='1' && !vis[i][j]){
+                 dfs(grid,i,j,vis);
+                 c++;
+                 
+               }
             }
         }
-        return island;
+       return c;  
     }
-    int[] delrow={-1,0,1,0};
-    int delcol[]={0,1,0,-1};
-    public void dfs(char[][] grid,boolean vis[][],int r,int c){
-        int n=grid.length;
-        int m=grid[0].length;
-        vis[r][c]=true;
-        for(int i=0;i<delrow.length;i++){
-             int nrow=r+delrow[i];
-             int ncol=c+delcol[i];
-             if(nrow>=0 && nrow<n && ncol>=0 && ncol<m &&grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
-                dfs(grid,vis,nrow,ncol);
-             }
+    int[] row={-1,0,1,0};
+    int[] col={0,1,0,-1};
+    private void dfs(char[][] grid,int i,int j,boolean[][] vis){
+      vis[i][j]=true;
+      int n=grid.length;
+      int m=grid[0].length;
+      for(int k=0;k<4;k++){
+        int nr=i+row[k];
+        int nc=j+col[k];
+        if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='1' && !vis[nr][nc]){
+            dfs(grid,nr,nc,vis);
+            
         }
+      }
     }
 }
