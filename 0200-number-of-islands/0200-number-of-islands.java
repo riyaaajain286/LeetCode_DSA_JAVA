22e6eaa146a6c96dc29f1e22ac15915427c2db9a
Riya Jain <riyaaajain286@gmail.com>
2026-08-14 17:42:55 +0530

    Time: 7 ms (13.72%), Space: 51.3 MB (92.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,33 +1,51 @@
 class Solution {
+
+    static int[] row={-1,0,1,0};
+    static int[] col={0,1,0,-1};
+
+    static class Pair{
+      int row,col;
+      public Pair(int r,int c){
+        this.row=r;
+        this.col=c;
+      }
+    } 
+
     public int numIslands(char[][] grid) {
-        int n=grid.length;
+       int n=grid.length;
         int m=grid[0].length;
         boolean[][] vis=new boolean[n][m];
         int c=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                if(grid[i][j]=='1' && !vis[i][j]){
-                 dfs(grid,i,j,vis);
+                 bfs(grid,i,j,vis);
                  c++;
-                 
-               }
+                }
             }
         }
        return c;  
     }
-    int[] row={-1,0,1,0};
-    int[] col={0,1,0,-1};
-    private void dfs(char[][] grid,int i,int j,boolean[][] vis){
-      vis[i][j]=true;
+
+    private static void bfs(char[][] grid,int i,int j,boolean[][] vis){
       int n=grid.length;
       int m=grid[0].length;
-      for(int k=0;k<4;k++){
-        int nr=i+row[k];
-        int nc=j+col[k];
-        if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='1' && !vis[nr][nc]){
-            dfs(grid,nr,nc,vis);
-            
+      vis[i][j]=true;
+      Queue<Pair> q=new LinkedList<>();
+      q.add(new Pair(i,j));
+      while(!q.isEmpty()){
+        int r=q.peek().row;
+        int c=q.peek().col;
+        q.poll();
+        for(int k=0;k<4;k++){
+          int nr=r+row[k];
+          int nc=c+col[k];
+          if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc]=='1' && !vis[nr][nc]){
+            vis[nr][nc]=true;
+            q.add(new Pair(nr,nc));
+          }
         }
       }
     }
+
 }
