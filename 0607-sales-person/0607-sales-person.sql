8e5e41200171462a9fe4011a40002c02a4d89018
Riya Jain <riyaaajain286@gmail.com>
2026-08-16 01:18:45 +0530

    Time: 1890 ms (15.59%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -2,10 +2,8 @@
 SELECT name 
 FROM SalesPerson 
 WHERE sales_id NOT IN(
-SELECT s.sales_id
-FROM SalesPerson s
-INNER JOIN Orders o
-ON s.sales_id=o.sales_id
+SELECT o.sales_id
+FROM  Orders o
 INNER JOIN Company c
 ON c.com_id=o.com_id
 WHERE c.name="RED");
