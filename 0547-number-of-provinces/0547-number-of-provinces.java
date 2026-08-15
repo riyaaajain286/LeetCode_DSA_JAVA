8d7d00a1e6e3ab9484d2972f589b257838b64434
Riya Jain <riyaaajain286@gmail.com>
2026-08-15 23:30:00 +0530

    Time: 2 ms (41.29%), Space: 47.1 MB (88.47%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -4,18 +4,18 @@ class Solution {
         boolean[] vis=new boolean[n];
         int provinces=0;
         for(int i=0;i<n;i++){ 
-        if(!vis[i]){
+          if(!vis[i]){
             Queue<Integer> q=new LinkedList<>();
             q.offer(i);
             while(!q.isEmpty()){
-            int node=q.poll();
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 && !vis[j]){
-                vis[j]=true;
-                q.offer(j);
+                int node=q.poll();
+                for(int j=0;j<n;j++){
+                    if(isConnected[node][j]==1 && !vis[j]){
+                    vis[j]=true;
+                    q.offer(j);
+                    }
                 }
-            }
-         }
+           }
             provinces++;
         }
         }
