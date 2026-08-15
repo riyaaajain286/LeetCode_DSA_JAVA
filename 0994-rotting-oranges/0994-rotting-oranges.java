5cd7f19dc48a26e7be5f5925d7d40ddb0b9eb30f
Riya Jain <riyaaajain286@gmail.com>
2026-08-16 00:14:24 +0530

    Time: 2 ms (89.66%), Space: 43.8 MB (92.49%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,43 +1,51 @@
 class Solution {
-    int[] r={-1,0,1,0};
-    int[] c={0,1,0,-1};
+    static class Pair{
+    int i,j;
+    public Pair(int i,int j){
+      this.i=i;
+      this.j=j;
+    }
+  }
+   static int[] drow={-1,0,1,0};
+   static int[] dcol={0,1,0,-1};
     public int orangesRotting(int[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
         int fresh=0;
-        Queue<int[]> q=new LinkedList<>();
-        //Put all rotten oranges in queue
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                   q.add(new int[]{i,j});
-                }
-                else if(grid[i][j]==1){
-                    fresh++;
-                }
+        Queue<Pair> q=new LinkedList<>();
+        // Put ALL rotten oranges into queue
+        for(int i=0;i<n;i++){ 
+         for(int j=0;j<m;j++){
+            if(grid[i][j]==2){
+            q.offer(new Pair(i,j));
             }
+            else if(grid[i][j]==1){
+            fresh++;
+            }
+         }
         }
         int minutes=0;
-        //bfs
+        //BFS
         while(!q.isEmpty() && fresh>0){
             int size=q.size();
-            for(int i=0;i<size;i++){
-                int[] curr=q.poll();
-                int x=curr[0];
-                int y=curr[1];
+            // Process one complete level
+            for(int l=0;l<size;l++){
+                int r=q.peek().i;
+                int c=q.peek().j;
+                q.poll();
                 for(int k=0;k<4;k++){
-                int nr=x+r[k];
-                int nc=y+c[k];
-                  if(nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc]==1){
+                int nr=r+drow[k];
+                int nc=c+dcol[k];
+                if(nr>=0 && nr<n && nc>=0 && nc<m &&grid[nr][nc]==1){
+                    q.add(new Pair(nr,nc));
                     grid[nr][nc]=2;
-                    q.add(new int[]{nr,nc});
                     fresh--;
-                  }
+                }
                 }
             }
             minutes++;// one level = one minute
         }
-        
+        // If fresh oranges are still left, they can never rot
         return fresh==0?minutes:-1;
     }
 }
