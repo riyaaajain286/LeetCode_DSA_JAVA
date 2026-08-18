f5915b6f1a3084e8a8a65694d210829dba875ba8
Riya Jain <riyaaajain286@gmail.com>
2026-08-18 22:45:57 +0530

    Time: 15 ms (42.12%), Space: 48.8 MB (75.19%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,44 +1,46 @@
 class Solution {
-  class Pair{
-    int row,col,steps;
-    public Pair(int row,int col,int steps){
-      this.row=row;
-      this.col=col;
-      this.steps=steps;
+
+     static class Pair{
+      int row,col,dist;
+      public Pair(int r,int c,int d){
+        this.row=r;
+        this.col=c;
+        this.dist=d;
+      }
     }
-   }
+
     public int[][] updateMatrix(int[][] mat) {
-        int n=mat.length;
-        int m=mat[0].length;
-        int[][] distance=new int[n][m];
-        int[][] visited=new int[n][m];
-        Queue<Pair>q=new LinkedList<>();
-        //push all cells with 1 into queue
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
-                q.offer(new Pair(i,j,0));
-                visited[i][j]=1;
-                }
+      int n=mat.length;
+      int m=mat[0].length;
+      int[][] dis=new int[n][m];
+      boolean [][] vis=new boolean[n][m];
+      Queue<Pair> q=new LinkedList<>();
+      //push all cells with 0 into the q
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+          if(mat[i][j]==0){
+            q.offer(new Pair(i,j,0));
+            vis[i][j]=true;
             }
         }
-        int[] delrow={-1,0,1,0};
-        int[] delcol={0,1,0,-1};
-        while(!q.isEmpty()){
-            int row=q.peek().row;
-            int col=q.peek().col;
-            int steps=q.peek().steps;
-            q.poll();
-            distance[row][col]=steps;
-            for(int k=0;k<4;k++){
-                int nrow=row+delrow[k];
-                int ncol=col+delcol[k];
-                if(nrow>=0&&ncol>=0&&nrow<n&&ncol<m&&visited[nrow][ncol]==0){
-                visited[nrow][ncol]=1;
-                q.add(new Pair(nrow,ncol,steps+1));
-                }
-            }
+      }
+      int[] drow={-1,0,1,0};
+      int[] dcol={0,1,0,-1};
+      while(!q.isEmpty()){
+        int r=q.peek().row;
+        int c=q.peek().col;
+        int dist=q.peek().dist;
+        dis[r][c]=dist;
+        q.poll();
+        for(int k=0;k<4;k++){
+           int nr=r+drow[k];
+           int nc=c+dcol[k];
+           if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc]){
+            vis[nr][nc]=true;
+            q.add(new Pair(nr,nc,dist+1));
+           }
         }
-        return distance;
-        }
+      }
+      return dis;
+    }
 }
