bfe212c2db183eb393a5ee18f3e809aa83bd2d02
Riya Jain <riyaaajain286@gmail.com>
2026-08-18 19:40:12 +0530

    Time: 2 ms (72.63%), Space: 44.5 MB (83.51%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -2,34 +2,26 @@ class Solution {
     public int largestInteger(int[] nums, int k) {
         int n=nums.length;
         int ans=-1;
-        int l=0;
-        // Number of windows in which each number appears
+       // Number of windows in which each number appears
         int[] freq=new int[51];
-        for(int r=l;r<n;r++){
-          
-          if(r-l+1==k){
+        for(int l=0;l<=n-k;l++){
              // Store which elements are present in this window
             boolean[] seen=new boolean[51];
-            for(int i=l;i<=r;i++){
+            for(int i=l;i<l+k;i++)
                 seen[nums[i]]=true;
-            }
+            
              // Count this window for every distinct element
              for(int i=0;i<=50;i++){
-                if(seen[i]){
+                if(seen[i])
                     freq[i]++;
-                }
              }
-             // Move left pointer
-             l++;
-            
           }
-        }
+        
          // Find largest element appearing in exactly one window
          for(int i=0;i<=50;i++){
-            if(freq[i]==1){
+            if(freq[i]==1)
                 ans=i;
-            }
-         }
+         }  
         return ans;
     }
 }
