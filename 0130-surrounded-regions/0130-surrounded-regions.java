37de826891425d1e7b03f87e0acafeb39e2142bc
Riya Jain <riyaaajain286@gmail.com>
2026-09-02 19:46:00 +0530

    Time: 2 ms (75.83%), Space: 48 MB (10.61%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,49 +1,62 @@
 class Solution {
+    //here the approach is that initially we are checking teh boundary that is first and last col and row then then using a visited array marking all O to 1 which are at boundary initially and then through other we check if vis is not 1 means 0 and board is O means inner matrix then mark it as X
     public void solve(char[][] board) {
         int n=board.length;
         int m=board[0].length;
-        int[][] vis=new int[n][m];
-        //check for boundary if any "O" present then check through dfs all its conenctions otherwise all others will be automatically be "X"
-        //first row
+        int[][] visited=new int[n][m];
+        //first row boundar check if O then mark visited as 1
         for(int j=0;j<m;j++){
-            if(vis[0][j]==0&&board[0][j]=='O'){
-                dfs(0,j,vis,board);
+            if(visited[0][j]==0 && board[0][j]=='O'){
+                dfs(board,0,j,visited);
+              
             }
-          //last row
-            if(vis[n-1][j]==0&&board[n-1][j]=='O'){
-                dfs(n-1,j,vis,board);
-            }
+               
         }
-        //first column
+
+        for(int j=0;j<m;j++){
+            if(visited[n-1][j]==0 && board[n-1][j]=='O'){
+                dfs(board,n-1,j,visited);
+               
+            }
+              
+        }
+
         for(int i=0;i<n;i++){
-            if(vis[i][0]==0&&board[i][0]=='O'){
-                dfs(i,0,vis,board);
-            }
-          //last column
-            if(vis[i][m-1]==0&&board[i][m-1]=='O'){
-                dfs(i,m-1,vis,board);
+            if(visited[i][0]==0 && board[i][0]=='O'){
+                dfs(board,i,0,visited);
+                
+            }    
+        }
+
+        for(int i=0;i<n;i++){
+            if(visited[i][m-1]==0 && board[i][m-1]=='O'){
+                dfs(board,i,m-1,visited);
+                
             }
         }
+       //this works at last to finally convert inner O to X
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]=='O'&&vis[i][j]==0){
+                if(board[i][j]=='O' && visited[i][j]==0){
                     board[i][j]='X';
+                    
                 }
             }
         }
     }
-    public void dfs(int row,int col,int[][] vis,char[][] board){
-        vis[row][col]=1;
-        int n=board.length;
-        int m=board[0].length;
-        int[] delrow={-1,0,1,0};
-        int[] delcol={0,1,0,-1};
-        for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&vis[nrow][ncol]==0&&board[nrow][ncol]=='O'){
-                dfs(nrow,ncol,vis,board);
-            }
+    static int[] drow={-1,0,1,0};
+    static int[] dcol={0,1,0,-1};
+    public static void dfs(char[][] board, int i, int j,int[][] visited){
+      int n=board.length;
+      int m=board[0].length;
+      visited[i][j]=1;
+      for(int k=0;k<4;k++){
+        int nrow=i+drow[k];
+        int ncol=j+dcol[k];
+        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]==0 && board[nrow][ncol]=='O' ){
+            
+            dfs(board,nrow,ncol,visited);
         }
+      }
     }
 }
