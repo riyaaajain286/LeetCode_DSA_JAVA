7cc282bfc9bacf2ad6440e229248701fdaa470ec
Riya Jain <riyaaajain286@gmail.com>
2026-09-03 12:33:57 +0530

    Time: 7 ms (29.91%), Space: 46.8 MB (67.08%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,37 +1,38 @@
 class Solution {
-    //kahn's algo
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+      ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+      int[] ans=new int[numCourses];
+      int ind=0;
         int[] indegree=new int[numCourses];
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] i:prerequisites){
-           adj.get(i[1]).add(i[0]);
-           indegree[i[0]]++;
+        for(int[] it:prerequisites){
+           adj.get(it[1]).add(it[0]);
+           indegree[it[0]]++;
         }
-        Queue<Integer> q=new LinkedList<>();
-        for(int j=0;j<numCourses;j++){
-            if(indegree[j]==0){
-                q.add(j);
-            }
-        }
-        int ind=0;
-        int[] ans=new int[numCourses];
-        while(!q.isEmpty()){
-            int node=q.poll();
-            ans[ind++]=node;
-             for(int adjnode:adj.get(node)){
-                indegree[adjnode]--;
-               if(indegree[adjnode]==0){
-                q.add(adjnode);
-               }
-            }
-        }
-        if(ind==numCourses) 
-         return ans;
-       return new int[0];
+     
+     Queue<Integer> q=new LinkedList<>();
+     
+     for(int i=0;i<numCourses;i++){
+      if(indegree[i]==0){
+         q.add(i);
         
-       
+      }
+     }
+    int c=0;
+     while(!q.isEmpty()){
+      int node=q.poll();
+      ans[ind++]=node;
+      for(int it:adj.get(node)){
+          indegree[it]--;
+          if(indegree[it]==0){
+            q.add(it);
+          }
+      }
+     }
+     if(ind==numCourses)
+      return ans;
+     return new int[0];
     }
 }
