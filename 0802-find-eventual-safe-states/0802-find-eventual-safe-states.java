938048aa5b946c5fcd26bb97b75715b1f9c952a0
Riya Jain <riyaaajain286@gmail.com>
2026-09-04 12:58:29 +0530

    Time: 27 ms (31.68%), Space: 60.7 MB (72.6%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,37 +1,34 @@
 class Solution {
-    //reverse graph+kahn's algo
     public List<Integer> eventualSafeNodes(int[][] graph) {
+        ArrayList<ArrayList<Integer>> revAdj=new ArrayList<>();
         int n=graph.length;
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
         for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
+            revAdj.add(new ArrayList<>());
         }
         int[] indegree=new int[n];
         for(int u=0;u<n;u++){
             for(int v:graph[u]){
-                adj.get(v).add(u);
+                revAdj.get(v).add(u);
                 indegree[u]++;
             }
         }
-        List<Integer> list=new ArrayList<>();
+        List<Integer> ans =new ArrayList<>();
         Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.add(i);
-            }
+            if(indegree[i]==0)
+             q.add(i);
         }
         while(!q.isEmpty()){
-            int node=q.remove();
-            list.add(node);
-            for(int it:adj.get(node)){
-                 indegree[it]--;
-                 if(indegree[it]==0){
+           int node=q.poll();
+           ans.add(node);
+           for(int it:revAdj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
                     q.add(it);
-                 }
-                
-            }
+                }
+           }
         }
-        Collections.sort(list);
-       return list; 
+        Collections.sort(ans);
+        return ans;
     }
 }
