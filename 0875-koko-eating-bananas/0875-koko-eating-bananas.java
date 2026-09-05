6fd0970f250ce371ca9c6249d32e4bbb85bcdf97
Riya Jain <riyaaajain286@gmail.com>
2026-09-05 17:04:54 +0530

    Time: 20 ms (31.18%), Space: 47.9 MB (43.53%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,31 +1,34 @@
 class Solution {
     //in this answer lies in range therefore use BS on answer
     public int minEatingSpeed(int[] piles, int h) {
-        int s=1;
-        int e=0;
-        for(int p:piles){
-            e=Math.max(e,p);
-        }
-        int ans=e;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            long totalhr=calcHours(piles,mid);
-            if(totalhr<=h){
-                ans=mid;
-                e=mid-1;
-            }
-            else s=mid+1;
-        }
-        return ans;
+        int low=1;
+        int high=0;
+
+      // find max pile
+     for(int p:piles){
+      high=Math.max(p,high);
+     }
+     int ans=high;
+     while(low<=high){
+      int mid=low+(high-low)/2;
+      long eatingHours=calculateHours(piles, mid);
+      if(eatingHours<=h){
+        ans=mid;
+        high=mid-1; // try smaller speed
+      }
+      else{
+        low=mid+1;// need faster speed
+      }
     }
-    private long calcHours(int[] piles,int hr){
-        
-         long total=0;
-        for(int p:piles){
-            //formula for ceil
-          total+=(p+hr-1)/hr;
-        }
-        
-        return total;
+    return ans;
+  }
+  
+  private static long calculateHours(int[] piles,int speed){
+    long totalHours=0;
+    for(int p:piles){
+       totalHours+=(int)Math.ceil((double)p/speed);
+    //   totalHours+=(p+speed-1)/speed;
+    }
+    return totalHours;
     }
 }
