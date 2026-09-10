a28813c467cbdda0fd7b9b1f75370b00eeddbe86
Riya Jain <riyaaajain286@gmail.com>
2026-09-10 23:06:52 +0530

    Time: 8 ms (41.04%), Space: 44.2 MB (15.72%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -6,14 +6,14 @@ class Solution {
         for(int i=0;i<n;i++){
             map.put(s.charAt(i),i);
         }
-        int left=0,end=0;
+        int start=0,end=0;
         for(int right=0;right<n;right++){
              // Extend partition if this character occurs later
              end=Math.max(end,map.get(s.charAt(right)));
               // Partition is complete
               if(right==end){
-                list.add(right-left+1);
-                left=end+1;
+                list.add(right-start+1);
+                start=end+1;
               }
         }
         return list;
