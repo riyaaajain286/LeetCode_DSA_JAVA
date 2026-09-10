9c0f7d3387f61cca5a10264b17c2e8cb2024cac0
Riya Jain <riyaaajain286@gmail.com>
2026-09-10 22:48:44 +0530

    Time: 9 ms (33.36%), Space: 44.4 MB (6.84%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int n=s.length();
+        List<Integer> list=new ArrayList<>();
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.put(s.charAt(i),i);
+        }
+        int left=0,end=0;
+        for(int right=0;right<n;right++){
+             // Extend partition if this character occurs later
+             end=Math.max(end,map.get(s.charAt(right)));
+              // Partition is complete
+              if(right==end){
+                list.add(right-left+1);
+                left=end+1;
+              }
+        }
+        return list;
+    }
+}
