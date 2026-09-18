b50c3b8efe829dbfe3992fc664babf08a48d2a27
Riya Jain <riyaaajain286@gmail.com>
2026-09-18 13:03:42 +0530

    Time: 798 ms (14.24%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -2,7 +2,5 @@
 SELECT c.name AS Customers
 FROM Customers c
 WHERE c.id NOT IN(
-SELECT c.id
-FROM Customers c
-INNER JOIN Orders o
-WHERE c.id=o.customerId);
+SELECT o.customerId
+FROM  Orders o);
